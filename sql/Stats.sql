ec750f9b40f250f2518f9dd5186ee1618ee7604f
Olivia <oliviabrz@gmail.com>
2021-06-21 12:48:05 -0500

    adding sql folder

---
+++ sql/Stats.sql
@@ -0,0 +1,53 @@
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('ast', 'assist')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('blk', 'block')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('dreb', 'defensive rebound')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('fg3pct', '3 point field goal percentage')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('fg3a', '3 point field goal attempt')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('fg3m', '3 point field goals made')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('fgpct', 'field goal percentage')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('fga', 'field goal attempt')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('fgm', 'field goals made')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('ftpct', 'free throw percentage')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('fta', 'free throw attempt')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('ftm', 'free throws made')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('min', 'minutes played')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('oreb', 'offensive rebound')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('pf', 'personal fouls')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('pts', 'points')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('reb', 'rebounds')
+
+INSERT into StatsAbbreviations (Abbreviation, Description)
+Values ('stl', 'steals')
