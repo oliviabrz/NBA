90c4f833979764f94ae4ac69c333f785fca09245
Olivia <oliviabrz@gmail.com>
2021-06-24 18:51:23 -0500

    saving updated play script

--- sql/Play.sql
+++ sql/Play.sql
@@ -4,26 +4,26 @@ DELETE from NBA.Player
 
 select COUNT(*) from NBA.Team;
 
-select * from NBA.Team
-
-select * from NBA.Player
-
 select COUNT(*) from NBA.Player;
 
-insert into NBA.Team
-(ID, Abbreviation, City, Conference, Division, FullName, Name)
-values (1,'ab', 'ab', 'ab', 'ab', 'ab', 'ab')
+select * from NBA.Team
+
+select * from NBA.Player 
 
 select ID from NBA.Team where Abbreviation = 'LAC'
-
-select ID from NBA.Team where Abbreviation = 'IND'
 
 insert into NBA.Player
         (ID, FirstName, LastName, Position, HeightFeet, HeightInches, WeightPounds,TeamID)
         values (2, 'Ike', 'Anigbogu', 'C', NULL, NULL, NULL, 1234) 
         
-select p.FirstName, p.LastName, t.FullName 
+select p.FirstName, p.LastName, p.HeightFeet, p.HeightInches, p.WeightPounds, t.FullName 
 from Player p
 inner join Team t  
 	on p.TeamID = t.ID 
-where p.LastName = 'Antetokounmpo';
+where p.LastName = 'Morris'
+and t.FullName in ('LA Clippers', 'Los Angeles Lakers');
+--and (t.FullName = 'LA Clippers' or t.FullName = 'Los Angeles Lakers');
+
+
+
+
